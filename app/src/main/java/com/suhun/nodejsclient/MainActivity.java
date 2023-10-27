c740826e057dbbe081897edc475d9f53c7e239a9
suhunlin <lin.suhun@msa.hinet.net>
2023-10-27 22:27:25 +0800

    1.Add layout 2.Initial layout

--- app/src/main/java/com/suhun/nodejsclient/MainActivity.java
+++ app/src/main/java/com/suhun/nodejsclient/MainActivity.java
@@ -3,12 +3,25 @@ package com.suhun.nodejsclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
+    private TextView resResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        resResult = findViewById(R.id.lid_resResult);
+    }
+
+    public void sendRequestFun(View view){
+
     }
 }
